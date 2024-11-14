5e21b9ff5946a8d1b83b2cb0a300d7839e77ab1a
neroil <edwin.haffner@heig-vd.ch>
2024-11-14 16:23:20 +0100

    database c bon

---
+++ app/src/main/java/ch/heigvd/iict/daa/template/database/note/NoteDatabase.kt
@@ -0,0 +1,62 @@
+package ch.heigvd.iict.daa.template.database.note
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
+import ch.heigvd.iict.daa.labo4.models.Note
+import kotlin.concurrent.thread
+
+@Database(entities = [Note::class],
+    version = 1,
+    exportSchema = true)
+@TypeConverters(NoteConverter::class)
+abstract class NoteDatabase : RoomDatabase() {
+
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+        private var INSTANCE : NoteDatabase? = null
+
+
+        fun getDatabase(context: Context) : NoteDatabase {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE = Room.databaseBuilder(context.applicationContext,
+                    NoteDatabase::class.java, "notedatabase.db")
+                    .fallbackToDestructiveMigration()
+                    .addCallback(NoteDatabaseCallBack())
+                    .build()
+                INSTANCE!!
+            }
+        }
+    }
+
+    class NoteDatabaseCallBack : RoomDatabase.Callback() {
+
+
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                thread {
+                    val isEmpty = database.noteDao().getNbNotes().equals(0)
+
+                    if(isEmpty) {
+                        //TODO : Mettre truc par défaut
+                        val note = Note.generateRandomNote()
+                        val schedule = Note.generateRandomSchedule()
+                        database.noteDao().insert(note)
+                        schedule?.let {
+                            database.noteDao().insert(it)
+                        }
+
+                    }
+                }
+            }
+        }
+        override fun onOpen(db: SupportSQLiteDatabase) { super.onOpen(db) }
+        override fun onDestructiveMigration(db: SupportSQLiteDatabase) { super.onDestructiveMigration(db) }
+    }
+}
---
+++ app/src/main/java/ch/heigvd/iict/daa/template/database/note/NoteRepository.kt
@@ -0,0 +1,29 @@
+package ch.heigvd.iict.daa.template.database.note
+
+import ch.heigvd.iict.daa.labo4.models.Note
+import ch.heigvd.iict.daa.labo4.models.Schedule
+import kotlin.concurrent.thread
+
+class NoteRepository(private val noteDao: NoteDao) {
+    val allNotes = noteDao.getAllNotes()
+
+    val allSchedule = noteDao.getAllNoteAndSchedule()
+
+    fun insertNotes(vararg notes: Note) {
+        thread {
+            noteDao.insertAllNotes(*notes)
+        }
+    }
+
+    fun insertSchedule(vararg schedules: Schedule) {
+        thread {
+            noteDao.insertAllSchedules(*schedules)
+        }
+    }
+
+    fun deleteAll(){
+        thread {
+            noteDao.deleteAllNotes()
+        }
+    }
+}
