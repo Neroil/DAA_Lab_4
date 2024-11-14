fdbb2ddde3c7f77efd3050d63c1d20c4bf9905c8
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2024-11-14 16:10:48 +0100

    ajout menu

--- app/src/main/java/ch/heigvd/iict/daa/template/MainActivity.kt
+++ app/src/main/java/ch/heigvd/iict/daa/template/MainActivity.kt
@@ -1,9 +1,26 @@
 package ch.heigvd.iict.daa.template
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    override fun onCreateOptionsMenu (menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu , menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected (item: MenuItem): Boolean{
+        return when (item. itemId ){
+            R.id.main_menu_generate -> { /* do something */ true}
+            R.id.main_menu_delete -> { /* do something */ true}
+            R.id.sort_menu_creation_date -> {true}
+            R.id.sort_menu_eta -> {true}
+            else -> super.onOptionsItemSelected (item)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
