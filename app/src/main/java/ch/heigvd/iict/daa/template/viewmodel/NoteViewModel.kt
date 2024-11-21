c59b77309b59b2f47b39734f255ac4ad393a2f2c
Tasticoco <114155288+Tasticoco@users.noreply.github.com>
2024-11-21 15:44:47 +0100

    ViewModel

--- app/src/main/java/ch/heigvd/iict/daa/template/viewmodel/NoteViewModel.kt
+++ app/src/main/java/ch/heigvd/iict/daa/template/viewmodel/NoteViewModel.kt
@@ -1,26 +1,34 @@
 package ch.heigvd.iict.daa.template.viewmodel
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ch.heigvd.iict.daa.labo4.models.Note
+import ch.heigvd.iict.daa.labo4.models.NoteAndSchedule
+import ch.heigvd.iict.daa.labo4.models.Schedule
+import ch.heigvd.iict.daa.template.database.note.NoteRepository
 
-class NoteViewModel : ViewModel() {
+class NoteViewModel(private val repo:NoteRepository) : ViewModel() {
 
     val nbNotesAndSchedules : LiveData<Int>get() = _nbNotesAndSchedules
-    val allNotesAndSchedules : LiveData<List<Int>>get() = _allNotesAndSchedules
+    val allNotesAndSchedules : LiveData<List<NoteAndSchedule>>get() = _allNotesAndSchedules
 
-    private val _nbNotesAndSchedules = MutableLiveData(0) // TODO get from repo
-    private val _allNotesAndSchedules = MutableLiveData(List(1) { _ -> 0 }) // TODO get from repo
+    private val _allNotesAndSchedules = repo.allNoteAndSchedule
+    private val _nbNotesAndSchedules = repo.nbNotes
+
 
     fun generateANote() {
-
+        val note = Note.generateRandomNote()
+        repo.insertNotes(note)
     }
 
     fun generateASchedule() {
-
+        val schedule = Note.generateRandomSchedule()
+        if (schedule != null) {
+            repo.insertSchedule(schedule)
+        }
     }
 
     fun deleteAllNoteAndSchedules() {
-
+        repo.deleteAll()
     }
 }
