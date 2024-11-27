6177676ebb78b412742ae1412b7ca79928931352
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2024-11-27 16:36:17 +0100

    Association evenement au bouton

--- app/src/main/java/ch/heigvd/iict/daa/template/fragments/NoteFragment.kt
+++ app/src/main/java/ch/heigvd/iict/daa/template/fragments/NoteFragment.kt
@@ -16,12 +16,6 @@ import ch.heigvd.iict.daa.template.databinding.FragmentNoteBinding
 import ch.heigvd.iict.daa.template.viewmodel.NoteViewModel
 import ch.heigvd.iict.daa.template.viewmodel.factory.NoteVMFactory
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [NoteFragment.newInstance] factory method to
@@ -73,23 +67,4 @@ class NoteFragment : Fragment() {
         _binding = null
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NoteFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NoteFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
